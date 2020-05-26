423b311094351bfc14736c8a80250ad8f6a64950
ginger-kang <kdhoon07@gmail.com>
2020-05-26 23:05:40 +0900

    add style

--- client/src/routes/Game/Game.tsx
+++ client/src/routes/Game/Game.tsx
@@ -1,5 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const GameContainer = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+const LeftImageContainer = styled.div`
+    width: 600px;
+    height: 600px;
+    background: black; 
+    border-radius: 7px;
+    color: white;
+    font-size: 100px;
+    display: flex;
+    align-items: center;
+`;
+
+const RightImageContainer = styled.div`
+    width: 600px;
+    height: 600px;
+    border-radius: 7px;
+    background: black; 
+    color: white;
+    font-size: 100px;
+    display: flex;
+    align-items: center;
+`;
+
+const VersusImageContainer = styled.div`
+    font-size: 70px;
+    font-weight: 700;
+`;
+
 
 export default function Game() {
-    return <div>Game</div>;
+    return (
+        <GameContainer>
+            <LeftImageContainer>
+                Lorem Ipsum
+            </LeftImageContainer>
+            <VersusImageContainer>
+                <span style={{ color: 'black' }}>V</span>
+                <span style={{ color: 'black' }}>S</span>
+            </VersusImageContainer>
+            <RightImageContainer>
+                Lorem Ipsum
+            </RightImageContainer>
+        </GameContainer>
+    );
 }
--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,25 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link }from 'react-router-dom';
 
 const HomeContainer = styled.div`
+    position: absolute;
     top: 0;
+    bottom: 0;
+    right: 0;
     left: 0;
-    background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7));
+    background-color: black;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-itmes: center;
+    align-items: center;
 `;
 
-const MainTitle = styled.div`
-    
+const MainTitleContainer = styled.div`
+    color: white;
+    width: auto;
+    margin: 150px 0 15px 0;
+    padding: 15px;
+    display: flex;
+    justify-content: center;
+`;
+
+const MainTitleImage = styled.div`
+    font-size: 120px;
+    font-weight: 600;
+`;
+
+const ContentContatiner = styled.div`
+    font-size: 20px;
+    padding: 15px;
+    color: white;
+    display: flex;
+    justify-content: center;
+`;
+
+const StartButton = styled.button`
+    width: 200px;
+    padding: 7px;
+    margin-top: 50px;
+    border-radius: 10px;
+    font-size: 27px;
+    font-weight: 800;
+    color: white;
+    background: rgba(0,0,0,0.3);
+    transition: all 0.1s;
+
+    &:hover {
+        -ms-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
 `;
 
 export default function Home() {
     return (
         <HomeContainer>
-            <MainTitle title='SHOW ME THE STYLE'>
-            </MainTitle>
+            <MainTitleContainer>
+                <MainTitleImage>
+                    Lorem Ipsum
+                </MainTitleImage>
+            </MainTitleContainer>
+            <ContentContatiner>
+                ...
+            </ContentContatiner>
+            <Link to="/game" >
+                <StartButton>
+                    START
+                </StartButton>
+            </Link>
         </HomeContainer>
     );
 }
