a513d50520b8279228c31d12f38e6aea87c54602
winnyiee@naver.com <winnyiee@naver.com>
2020-05-27 21:32:04 +0900

    ff

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-//import Amplify from 'aws-amplify'
-//import config from './aws-exports'
-
-//Amplify.configure(config)
-
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const app = express();
 const cors = require('cors');
-const path = require('path')
+//const path = require('path')
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -16,9 +11,9 @@ app.use('/graphql',graphqlHTTP({
     schema,
     graphiql:true
 }));
-app.get('/*', (req, res) => {
+/*app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-  });
+  });*/
 
 const PORT = process.env.PORT || 5000;
 
