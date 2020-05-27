b4c204292340560a94e5fae2a3209d2c173b11fa
winnyiee@naver.com <winnyiee@naver.com>
2020-05-27 21:55:18 +0900

    remove postbuild something

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const app = express();
 const cors = require('cors');
-//const path = require('path')
+const path = require('path')
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -11,9 +11,9 @@ app.use('/graphql',graphqlHTTP({
     schema,
     graphiql:true
 }));
-/*app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-  });*/
+  });
 
 const PORT = process.env.PORT || 5000;
 
