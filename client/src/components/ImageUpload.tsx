f4df63a2ac95f6da0d8b0609bbbb93499608275c
winnyiee@naver.com <winnyiee@naver.com>
2020-06-04 23:03:49 +0900

    adding : file upload button

--- client/src/components/ImageUpload.tsx
+++ client/src/components/ImageUpload.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+
 import axios from 'axios';
+
+
 
 const FileUploadContainer = styled.div`
     width: 100%;
@@ -18,8 +21,10 @@ const Upload = styled.div`
 position: relative;
 max-width: 205px;
 margin: 50px auto;`;
-const Input = styled.div`
+
+const Input = styled.input`
 display: none;`;
+
 const Preview = styled.div`
 width: 192px;
             height: 192px;
@@ -28,7 +33,8 @@ width: 192px;
             border: 6px solid #F8F8F8;
             box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
 `;
-const Label = styled.div`
+const Label = styled.label`
+for:imageUpload
 display: inline-block;
 width: 34px;
 height: 34px;
@@ -40,14 +46,14 @@ box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
 cursor: pointer;
 font-weight: normal;
 transition: all .2s ease-in-out;
-
+    
 &:hover {
     background: #f1f1f1;
     border-color: #d6d6d6;
 }
 &:after {
-    content: "\f040";
-    font-family: 'FontAwesome';
+    icon: ;
+    font-family: 'fontawesome';
     color: #757575;
     position: absolute;
     top: 10px;
@@ -58,48 +64,52 @@ transition: all .2s ease-in-out;
 }`;
 
 
+
+
 function ImageUpload() {
 
     //It sends a request to upload to the server by storing the file object in the state
-    const [uploadedFile, setUploadedFile] = useState('');
-    const uploadInput = useRef(null);
-
-    //Post request to server when submitted 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        const imageData = new FormData();
-        imageData.append("file", uploadedFile);
-
-        //request
-        axios.post(
-            'S3 업로드할 url',
-            imageData
-        ).then((res) => {
-            console.log(res.data);
-        }).catch((error) => {
-            console.log(error);
-        });
+    const FileUpload() {
+        console.log(11);
+        const [uploadedFile, setUploadedFile] = useState('');
+        
+    
+        //Post request to server when submitted 
+        const handleSubmit = (e: any) => {
+            e.preventDefault();
+            const imageData = new FormData();
+            imageData.append("file", uploadedFile);
+    
+            //request
+            axios.post(
+                'S3 업로드할 url',
+                imageData
+            ).then((res) => {
+                console.log(res.data);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    
+        const handleClick = (e: any) => {
+            console.log(e.target);
+        };
+    
+        const handleChange = (e: any) => {
+            const uploadedFile = e.target.files[0];
+        };
     }
-
-    const handleClick = (e: any) => {
-        console.log(e.target);
-    };
-
-    const handleChange = (e: any) => {
-        const uploadedFile = e.target.files[0];
-    };
-
+    const uploadInput = useRef(null);
     return (
         <FileUploadContainer>
-           
+
             <Upload>
                 <Edit>
-                    <Input>
-                    </Input>
-                    <Label>                        
+                    <input ref={uploadInput} type='file' onSubmit={FileUpload} style={{display:'none'}}/>                    
+                    <Label>
                     </Label>
                 </Edit>
-                <Preview>                    
+                <Preview>
                 </Preview>
             </Upload>
 
